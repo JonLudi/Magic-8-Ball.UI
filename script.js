7e2f06fe6e9a6d6df9c247dad05eb572942f7d2d
JonLudi <144353376+JonLudi@users.noreply.github.com>
2023-11-09 15:15:22 +0000

    Magic 8 Ball work

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("askButton").addEventListener("click", function(){
+let answer ="";
+let randomIndex = Math.floor(Math.random()*5);
+if(randomIndex === 0){
+    answer = "It is certain";
+} else if (randomIndex === 1){
+    answer = "Ask Again Later";{
+  else if ()
+    }
+}
+});
