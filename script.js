42c4d579c2d28054df31abf964a796884bce649f
JonLudi <144353376+JonLudi@users.noreply.github.com>
2023-11-14 15:15:15 +0000

    Himethy

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
+//Create event Listener
 document.getElementById("askButton").addEventListener("click", function(){
-let answer;
+let answer = '';
+//Generate a random number between 0 and 4 (inclusive), rounded
 let randomIndex = Math.floor(Math.random()*5);
+
+//Use if-else-if to provide an answer based on the randomIndex
 if(randomIndex === 0){
     answer = "It is certain";
 } else if (randomIndex === 1){
     answer = "Ask Again Later";
 } else if (randomIndex === 2){
-  answer = "2";
+  answer = "Yes, Definitly!";
 } else if (randomIndex === 3){
-  answer ="3";
+  answer ="My reply is no.";
 } else if (randomIndex === 4){
-  answer = "4";
-} else if (randomIndex === 5){
-  answer = "5"
+  answer = "I cannot predict now.";
+} else {
+  answer = "Very Doubtful.";
+}
+});
+
+//Check if a question is asked, trim method checks for a blank input
+let question = document.getElementById('question').value;
+if (question.trim()=== ''){
+  alert("Please enter a question.")
+}else{
+  document.getElementById('answer').textContent = answer;
 }
 });
