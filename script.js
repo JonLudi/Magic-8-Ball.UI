9fbcf3e991861f8a0d028835ff84f1de04dbb8b4
JonLudi <144353376+JonLudi@users.noreply.github.com>
2023-11-14 12:18:59 +0000

    finished javascript

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 document.getElementById("askButton").addEventListener("click", function(){
-let answer ="";
+let answer;
 let randomIndex = Math.floor(Math.random()*5);
 if(randomIndex === 0){
     answer = "It is certain";
 } else if (randomIndex === 1){
-    answer = "Ask Again Later";{
-  else if ()
-    }
+    answer = "Ask Again Later";
+} else if (randomIndex === 2){
+  answer = "2";
+} else if (randomIndex === 3){
+  answer ="3";
+} else if (randomIndex === 4){
+  answer = "4";
+} else if (randomIndex === 5){
+  answer = "5"
 }
 });
